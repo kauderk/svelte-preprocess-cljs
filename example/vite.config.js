8b8918ff9af99729ece1e1c511808933f3d9c103
kauderk <estudio1kau@gmail.com>
2024-02-16 21:03:38 -0500

    squint svelte preprocess

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import {_main as cljsPreprocess} from "svelte-preprocess-cljs";
+import { compileString } from "squint-cljs/lib/compiler.js";
+
+const squint = () => ({
+  name: "svelte-plugin-squint",
+  script({ content, attributes: { lang } }) {
+    if (lang !== "cljs") return;
+
+    try {
+      const code = compileString(content);
+      return { code };
+    } catch (e) {
+      console.log(e);
+    }
+  },
+});
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte({
+		preprocess: [squint()],
+	})],
 });
